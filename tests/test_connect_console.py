2d38f44784dced6527b9f9aec3fdde9bde489e33
chukysoria <chukysoria@users.noreply.github.com>
2016-04-21 17:57:05 +0200

    Switch tox to pytest

--- tests/test_connect_console.py
+++ tests/test_connect_console.py
@@ -4,12 +4,38 @@ from tests import mock
 
 import sc_console
 
+from sc_console.connect import PlaybackSession
+
 import spotifyconnect
 
 
-class test_connect_console(unittest.TestCase):
+class Test_Connect(unittest.TestCase):
     
     def test_spotify_key_missing(self):
         
         with self.assertRaises(IOError):
             sc_console.Connect()
+
+class Test_PlaybackSession(unittest.TestCase):
+
+    def test_default(self):
+
+        playback_session = PlaybackSession()
+
+        self.assertFalse(playback_session.active)
+
+    def test_activate(self):
+
+        playback_session = PlaybackSession()
+        playback_session.activate()
+
+        self.assertTrue(playback_session.active)
+
+
+    def test_deactivate(self):
+
+        playback_session = PlaybackSession()
+        playback_session.activate()
+        playback_session.deactivate()
+
+        self.assertFalse(playback_session.active)
