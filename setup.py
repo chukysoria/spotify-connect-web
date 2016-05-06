229e9bb8fd7a983d995d55b85996f2d1d4bc7977
chukysoria <chukysoria@users.noreply.github.com>
2016-05-06 09:25:33 +0200

    Requirements update

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_version(filename):
 
 setup(
     name='sc-console',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/chukysoria/spotify-connect-web',
     license='Apache License, Version 2.0',
     author='chukysoria',
@@ -39,7 +39,7 @@ setup(
         'Flask-Bootstrap >= 3.3.2.1',
         'Flask-Cors >= 2.1.2',
         'pyalsaaudio >= 0.8',
-        'pyspotify-connect >= 0.1.0',
+        'pyspotify-connect >= 0.1.11',
         'six >= 1.10.0'],
     entry_points={
         'console_scripts': [
